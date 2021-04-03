a58483b49d9740ecf8f89782352016f37e62b13c
jsmithb117 <jsmithb117@gmail.com>
2021-04-03 11:40:40 -0700

    changed environment variables for database

--- db/index.js
+++ db/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 const mongoose = require('mongoose');
 // const instructorsSchema = require('./schemas/instructorsSchema');
 // const offeredBysSchema = require('./schemas/offeredBysSchema');
@@ -6,10 +7,8 @@ const { Instructors, OfferedBys, Testimonials } = require('./models.js');
 // const OfferedBys = require('./models/offeredBysModel');
 // const Testimonials = require('./models/testimonialsModel');
 
-const imagesPort = process.env.IMAGES_PORT;
-
-//returns all instructors for all courses
-findAllInstructors = (cb) => {
+// returns all instructors for all courses
+const findAllInstructors = (cb) => {
   Instructors.find()
     .then((dbResponse) => {
       cb(null, dbResponse);
@@ -22,9 +21,9 @@ findAllInstructors = (cb) => {
     });
 };
 
-//returns all instructors for a course
-findInstructors = (courseNumber, cb) => {
-  let options = {courses: {$elemMatch: { courseNumber }}};
+// returns all instructors for a course
+const findInstructors = (courseNumber, cb) => {
+  const options = { courses: { $elemMatch: { courseNumber } } };
   Instructors.find(options)
     .then((dbResponse) => {
       cb(dbResponse);
@@ -36,9 +35,9 @@ findInstructors = (courseNumber, cb) => {
     });
 };
 
-//returns the primary instructor for a course
-findPrimaryInstructor = (courseNumber, cb) => {
-  let options = {courses: {$elemMatch: { courseNumber, isPrimaryInstructor: true }}};
+// returns the primary instructor for a course
+const findPrimaryInstructor = (courseNumber, cb) => {
+  const options = { courses: { $elemMatch: { courseNumber, isPrimaryInstructor: true } } };
   Instructors.findOne(options)
     .then((dbResponse) => {
       cb(dbResponse);
@@ -51,9 +50,9 @@ findPrimaryInstructor = (courseNumber, cb) => {
     });
 };
 
-//returns all offeredBy documents for all courses
+// returns all offeredBy documents for all courses
 
-findAllOfferedBys = (cb) => {
+const findAllOfferedBys = (cb) => {
   OfferedBys.find()
     .then((dbResponse) => {
       cb(null, dbResponse);
@@ -66,9 +65,9 @@ findAllOfferedBys = (cb) => {
     });
 };
 
-//returns the offeredBy for a course
-findOfferedBy = (courseNumber, cb) => {
-  let options = { id: courseNumber };
+// returns the offeredBy for a course
+const findOfferedBy = (courseNumber, cb) => {
+  const options = { id: courseNumber };
 
   OfferedBys.find(options)
     .then((dbResponse) => {
@@ -82,18 +81,18 @@ findOfferedBy = (courseNumber, cb) => {
     });
 };
 
-//returns three random testimonials
-findTestimonials = (courseNumber, cb) => {
-  let testimonialsIndexes = [];
+// returns three random testimonials
+const findTestimonials = (courseNumber, cb) => {
+  const testimonialsIndexes = [];
   while (testimonialsIndexes.length < 3) {
-    let random = Math.floor(Math.random() * 9);
+    const random = Math.floor(Math.random() * 9);
     if (!testimonialsIndexes.includes(random)) {
       testimonialsIndexes.push(random);
     }
   }
   Testimonials.find()
     .then((dbResponse) => {
-      let output = [];
+      const output = [];
       testimonialsIndexes.forEach((index) => output.push(dbResponse[index]));
       cb(output);
       // mongoose.connection.close();
@@ -105,4 +104,11 @@ findTestimonials = (courseNumber, cb) => {
     });
 };
 
-module.exports = { findAllInstructors, findInstructors, findPrimaryInstructor, findOfferedBy, findTestimonials, findAllOfferedBys };
+module.exports = {
+  findAllInstructors,
+  findInstructors,
+  findPrimaryInstructor,
+  findOfferedBy,
+  findTestimonials,
+  findAllOfferedBys,
+};
--- db/models.js
+++ db/models.js
@@ -1,19 +1,27 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const instructorsSchema = require('./schemas/instructorsSchema.js');
 const testimonialsSchema = require('./schemas/testimonialsSchema.js');
 const offeredBysSchema = require('./schemas/offeredBysSchema.js');
 
-mongoose.connect('mongodb://127.0.0.1/instructors', { useNewUrlParser: true, useUnifiedTopology: true });
+dotenv.config();
+const MONGO_HOSTNAME = process.env.MONGO_HOSTNAME || 'localhost';
+const MONGO_PORT = process.env.MONGO_PORT || 27017;
+const MONGO_DB_INSTRUCTORS = process.env.MONGO_DB_INSTRUCTORS || 'instructors';
+const MONGO_DB_OFFEREDBYS = process.env.MONGO_DB_OFFEREDBYS || 'offeredbys';
+const MONGO_DB_TESTIMONIALS = process.env.MONGO_DB_TESTIMONIALS || 'testimonials';
+
+mongoose.connect(`mongodb://${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB_INSTRUCTORS}`, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'instructors connection error'));
+db.on('error', console.error.bind(console, 'instructors db connection error'));
 db.once('open', () => {
   console.log('Connected to instructors db');
 });
 
-const Instructors = mongoose.model('instructors', instructorsSchema);
-const OfferedBys = mongoose.model('offeredbys', offeredBysSchema);
-const Testimonials = mongoose.model('testimonials', testimonialsSchema);
+const Instructors = mongoose.model(MONGO_DB_INSTRUCTORS, instructorsSchema);
+const OfferedBys = mongoose.model(MONGO_DB_OFFEREDBYS, offeredBysSchema);
+const Testimonials = mongoose.model(MONGO_DB_TESTIMONIALS, testimonialsSchema);
 
 module.exports.Instructors = Instructors;
 module.exports.OfferedBys = OfferedBys;
