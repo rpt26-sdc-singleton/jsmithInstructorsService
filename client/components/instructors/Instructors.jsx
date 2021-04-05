611198e2d996b4de9b454fca515652e3405211ad
jsmithb117 <jsmithb117@gmail.com>
2021-04-05 08:37:52 -0700

    Changed fetch urls

--- client/components/instructors/Instructors.jsx
+++ client/components/instructors/Instructors.jsx
@@ -1,17 +1,24 @@
+/* eslint-disable import/extensions */
+/* eslint-disable no-plusplus */
+/* eslint-disable no-undef */
+/* eslint-disable no-console */
 import React, { useState, useEffect } from 'react';
 import initialState from './initialState';
 import Instructor from './Instructor.jsx';
 
+const instructorsUrl = '127.0.0.1';
+const imagesUrl = '54.176.19.199';
+
 const Instructors = () => {
-  let splitPath = window.location.pathname.split('/')[1];
-  const course = splitPath ? splitPath : 1;
-  const [courseNumber, setCourseNumber] = useState(course);
+  const splitPath = window.location.pathname.split('/')[1];
+  const course = splitPath || 1;
+  const [courseNumber] = useState(course);
   const [instructorsData, setInstructorsData] = useState(initialState.instructorsData);
   const [primaryInstructorImage, setPrimaryInstructorImage] = useState('');
-  const [additionalInstructorImages, setAdditionalInstructorImages] = useState([{instructorId: 0, instructorImage: ''}]);
+  const [additionalInstructorImages, setAdditionalInstructorImages] = useState([{ instructorId: 0, instructorImage: '' }]);
   const [label, setLabel] = useState('Instructor');
   const [svgs, setSvgs] = useState(initialState.svgs);
-  const [windowSize, setWindowSize] = useState(window.innerWidth);
+  // const [windowSize, setWindowSize] = useState(window.innerWidth);
   const [gridClass, setGridClass] = useState('instructor-grid-small');
   const windowSizeGetter = () => {
     if (window.innerWidth <= 1040) {
@@ -20,12 +27,12 @@ const Instructors = () => {
       setGridClass('instructor-grid-large');
     }
   };
-  //GET and set instructors data for this course as state
+  // GET and set instructors data for this course as state
   useEffect(() => {
     windowSizeGetter();
     window.addEventListener('resize', windowSizeGetter);
 
-    fetch(`http://localhost:3003/api/instructors/${courseNumber}`)
+    fetch(`http://${instructorsUrl}:3003/api/instructors/${courseNumber}`)
       .then((response) => response.json())
       .then((json) => {
         setInstructorsData(json);
@@ -33,19 +40,19 @@ const Instructors = () => {
         setLabel(newLabel);
       })
       .catch((err) => { if (err) { console.error(err); } });
-    fetch(`http://localhost:3006/api/image/${courseNumber}/primaryInstructor`)
-      .then((response) =>response.json())
+    fetch(`http://${imagesUrl}:3006/api/image/${courseNumber}/primaryInstructor`)
+      .then((response) => response.json())
       .then((json) => {
         setPrimaryInstructorImage(json.primaryInstructor);
       })
       .catch((err) => { if (err) { console.error(err); } });
-    fetch(`http://localhost:3006/api/image/${courseNumber}/additionalInstructors`)
-      .then((response) =>response.json())
+    fetch(`http://${imagesUrl}:3006/api/image/${courseNumber}/additionalInstructors`)
+      .then((response) => response.json())
       .then((json) => {
         setAdditionalInstructorImages(json.additionalInstructors);
       })
       .catch((err) => { if (err) { console.error(err); } });
-    fetch('http://localhost:3006/api/svgs')
+    fetch(`http://${imagesUrl}:3006/api/svgs`)
       .then((response) => response.json())
       .then((json) => { setSvgs(json); })
       .catch((err) => { if (err) { console.error(err); } });
@@ -60,7 +67,7 @@ const Instructors = () => {
         {'Instructor rating '}
         <span>
           <svg className="instructors-svg" viewBox="0 0 90 90">
-            <path d={svgs.instructorSVG}></path>
+            <path d={svgs.instructorSVG} />
           </svg>
         </span>
         <span className="instructors-ratings">
@@ -68,24 +75,24 @@ const Instructors = () => {
         </span>
         <span>
           <svg className="instructors-infoSVG" viewBox="0 0 80 80" height="30px" width="30px">
-            <path d={svgs.infoSVG.i}></path>
-            <path d={svgs.infoSVG.dot}></path>
-            <polygon points={svgs.infoSVG.circle}></polygon>
+            <path d={svgs.infoSVG.i} />
+            <path d={svgs.infoSVG.dot} />
+            <polygon points={svgs.infoSVG.circle} />
           </svg>
         </span>
       </span>
       <div className="instructor-grid" id={gridClass}>
         {instructorsData.map((instructor, index) => {
-        //set image while mapping, pass as props
+        // set image while mapping, pass as props
           let isPrimary = false;
           let image = primaryInstructorImage;
-          //is instructor primary?
+          // is instructor primary?
           for (let i = 0; i < instructor.courses.length; i++) {
             if (instructor.courses[i].courseNumber === courseNumber) {
               isPrimary = true;
             }
           }
-          //if instructor is not primary, find and set image in additionalInstructors
+          // if instructor is not primary, find and set image in additionalInstructors
           if (!isPrimary) {
             for (let i = 0; i < additionalInstructorImages.length; i++) {
               if (instructor.id === additionalInstructorImages[i].instructorId) {
