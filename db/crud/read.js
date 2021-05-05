e9862589c5726982c9d08ecdad8013bd05567f5e
Josh Parker <joshBBparker@gmail.com>
2021-05-05 17:45:23 -0600

    update read function to find one record only

--- db/crud/read.js
+++ db/crud/read.js
@@ -1,6 +1,6 @@
 module.exports = (m, id) => new Promise((resolve, reject) => {
   m((db, model) => {
-    model.find({ id })
+    model.findOne({ id })
       .then((record) => resolve(record))
       .catch((err) => reject(err));
   });
