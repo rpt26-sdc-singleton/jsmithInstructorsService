62f5e3b130bc36d7860e878a742f71223fbc8f92
Josh Parker <joshBBparker@gmail.com>
2021-05-17 14:36:14 -0600

    revert instructor generator and update to generate instructors out of core

--- db/generators/instructorsGenerator.js
+++ db/generators/instructorsGenerator.js
@@ -1,56 +1,16 @@
 /* eslint-disable no-plusplus */
 /* eslint-disable no-nested-ternary */
 // eslint-disable-next-line import/no-extraneous-dependencies
-// const faker = require('faker');
-// const fs = require('fs');
+const faker = require('faker');
+const fs = require('fs');
 
 const numberToGenerate = 10000000; // ten million
-const estimatedMaxMemory = 1000000000; // one GB... mostly just a guess
 
-const mutatingShuffle = (arr) => {
-  for (let i = 0; i < arr.length; i++) {
-    const j = i + Math.floor(Math.random() * (arr.length - i));
-    const temp = arr[i];
-
-    // eslint sensibly warns you not to mutate your inputs, but I'm ignoring that here.
-    // Why? This function is meant to handle large arrays (10 million elements)
-    // therefore, I'm choosing to mutate the input for the sake of memory efficiency.
-
-    // eslint-disable-next-line no-param-reassign
-    arr[i] = arr[j];
-    // eslint-disable-next-line no-param-reassign
-    arr[j] = temp;
-  }
-};
-
-console.log(process.memoryUsage().rss / estimatedMaxMemory);
-
-const primaryInstructors = new Array(numberToGenerate);
-
-console.log(process.memoryUsage().rss / estimatedMaxMemory);
-
-for (let i = 0, j = 1; i < primaryInstructors.length; j++) {
-  const coursesForThisInstructor = i + Math.floor(6 * Math.random());
-  for (; i < coursesForThisInstructor; i++) {
-    primaryInstructors[i] = j;
-  }
-}
-
-console.log(process.memoryUsage().rss / estimatedMaxMemory);
-
-// console.log(primaryInstructors.slice(0, 20));
-// console.log(primaryInstructors.slice(numberToGenerate - 20));
-
-mutatingShuffle(primaryInstructors);
-
-// note: at this point, we're already using 67% of available memory...
-// it might be wise to write the result to the file system before proceeding.
-
-console.log(process.memoryUsage().rss / estimatedMaxMemory);
-
-/*
+// generating instructors out of core (it uses too much memory to do it in core)
+// reference here: https://stackoverflow.com/a/43370201/13920055
+// and here: https://nodejs.org/api/fs.html#fs_fs_createwritestream_path_options
 const generateInstructors = () => {
-  const instructors = [];
+  const instructors = fs.createWriteStream('./db/data/instructors', { flags: 'a' }); // a for append mode.
 
   const schools = [
     'Alfred University',
@@ -81,14 +41,8 @@ const generateInstructors = () => {
     'Zaytuna College',
   ]; // Pulled manually from https://nces.ed.gov/collegenavigator
 
-  // creates instructors
   const createInstructors = () => {
     for (let id = 1; id <= numberToGenerate; id++) {
-      // console.log('Instructors generating data for course ', id);
-      if (id % 100000 === 0 && process.memoryUsage.rss() > (0.5 * process.resourceUsage().maxRSS)) {
-
-      }
-
       let bool;
       const random = Math.random();
       let rating = random;
@@ -116,13 +70,14 @@ const generateInstructors = () => {
         instructorAverageRating: Number.parseFloat(rating).toPrecision(2),
         numberOfRatings: Math.floor(rating * random * 2345),
       };
+      instructors.write(`${JSON.stringify(instructor)}\n`);
       instructors.push(instructor);
     }
   };
 
   // adds one primary instructor per course
   const addPrimaryInstructors = () => {
-    for (let i = 1; i <= numberToGenerate; i++) {
+    for (let i = 1; i <= 100; i++) {
       const index = Math.floor(Math.random() * 40) + 1; // only first 40 as primaryInstructor
       const courseObj = {
         courseNumber: i,
@@ -142,7 +97,7 @@ const generateInstructors = () => {
 
   // assigns 0 to 3 assistant instructors per course, no min or max courses per assistant
   const addAssistantInstructors = () => {
-    for (let courseNumber = 1; courseNumber <= numberToGenerate; courseNumber++) {
+    for (let courseNumber = 1; courseNumber <= 100; courseNumber++) {
       const numberOfAssistants = Math.floor(Math.random() * 4);
       const assistants = [];
 
@@ -167,11 +122,8 @@ const generateInstructors = () => {
   createInstructors();
   addPrimaryInstructors();
   addAssistantInstructors();
-
   fs.writeFileSync('./db/data/instructors.json', JSON.stringify(instructors, null, '\t'));
-
 };
 
 module.exports = generateInstructors;
 generateInstructors();
-*/
